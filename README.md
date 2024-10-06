5d1da5b59700a57099e28b1d4222488085367770
Mattia Papa <mattiapapa@mattias-macbook-air.home>
2024-10-06 13:07:48 +0200

    formatter improved

--- README.md
+++ README.md
@@ -1,8 +1,60 @@
 # rust-complete-dev-guidee
 
+## Project description
+
 ## Definitions
+
 - Vector: can grow and shrink in size and contains elements
 - Array: it is a collection of elements fixed in size
+- Trait: a set of functions
 
-> ⚠️ In Rust "variables" are called "binìdings"
+> ⚠️ In Rust "variables" are called "bindings"
 
+## Useful
+
+### Formatter
+
+#### {:#}
+
+```
+#[derive(Debug)]
+
+// #[derive(Debug)] it is going to be used whenever we try to print `deck` using the Debug Formatter {:?}
+    println!("Here is your deck {:#?}", deck);
+```
+
+**TERMINAL (cargo run -q):**
+
+```
+Here is your deck Deck {
+    cards: [
+        "Ace of Hearts",
+        "Two of Hearts",
+        "Three of Hearts",
+        "Ace of Spades",
+        "Two of Spades",
+        "Three of Spades",
+        "Ace of Diamonds",
+        "Two of Diamonds",
+        "Three of Diamonds",
+    ],
+}
+```
+
+## Implementation
+
+Make a list of suits:
+
+- harts
+- spades
+- clubs
+- diamonds
+
+Make a list of values:
+
+- ace
+- two
+- three
+- ...
+
+Create a double nested for looping all possible combinations
--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,12 +1,34 @@
+#[derive(Debug)]
+
 struct Deck {
     cards: Vec<String>,
 }
 
 fn main() {
-    // Creates an instance of Deck struct, and inside that creates an empty vector `cards`
-    let deck: Deck = Deck { cards: vec![] };
-    // These 2 ways of creating a vector are equivalent
-    let deck1: Deck = Deck { cards: Vec::new() };
+    // Possible Suits
+    let suits = ["Hearts", "Spades", "Diamonds"];
+    // Possible Values
+    let values = ["Ace", "Two", "Three"];
 
-    println!("Here is your deck {}", deck);
+    // let card: String;
+    let mut cards: Vec<String> = vec![];
+
+    // Double nested for loop to get all possible combinations
+    for suit in suits {
+        for value in values {
+            let card: String = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    // Creates an instance of Deck struct, and inside that creates an empty vector `cards`
+    // let deck: Deck = Deck { cards: vec![] };
+    // Let's create the actual deck
+    let deck: Deck = Deck { cards: cards }; // Also let deck = Deck {cards}
+
+    // These 2 ways of creating a vector are equivalent
+    // let deck1: Deck = Deck { cards: Vec::new() };
+
+    // #[derive(Debug)] it is going to be used whenever we try to print `deck` using the Debug Formatter {:?}
+    println!("Here is your deck {:#?}", deck);
 }
