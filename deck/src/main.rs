51f0af215939a93aad27df162626f546f1b3e5a3
Mattia <mp.web3@gmail.com>
2024-10-07 10:48:45 +0200

    new branch

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -4,31 +4,29 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    // Possible Suits
-    let suits = ["Hearts", "Spades", "Diamonds"];
-    // Possible Values
-    let values = ["Ace", "Two", "Three"];
+// Inherent Implementation of Deck
+// Self is a reference to whatever is the parent implementation
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+        let mut cards: Vec<String> = vec![];
 
-    // let card: String;
-    let mut cards: Vec<String> = vec![];
-
-    // Double nested for loop to get all possible combinations
-    for suit in suits {
-        for value in values {
-            let card: String = format!("{} of {}", value, suit);
-            cards.push(card);
+        // Double nested for loop to get all possible combinations
+        for suit in suits {
+            for value in values {
+                let card: String = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+
+        let deck: Deck = Deck { cards };
+        return deck;
     }
+}
 
-    // Creates an instance of Deck struct, and inside that creates an empty vector `cards`
-    // let deck: Deck = Deck { cards: vec![] };
-    // Let's create the actual deck
-    let deck: Deck = Deck { cards: cards }; // Also let deck = Deck {cards}
+fn main() {
+    let deck = Deck::new();
 
-    // These 2 ways of creating a vector are equivalent
-    // let deck1: Deck = Deck { cards: Vec::new() };
-
-    // #[derive(Debug)] it is going to be used whenever we try to print `deck` using the Debug Formatter {:?}
     println!("Here is your deck {:#?}", deck);
 }
