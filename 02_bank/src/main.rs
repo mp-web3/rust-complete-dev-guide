8616be7706ba3311b8b27b5d525479ef2a6960e2
mp-web3 <mp.web3.t@gmail.com>
2024-10-10 07:26:53 +0200

    refactored as for the course

--- 02_bank/src/main.rs
+++ 02_bank/src/main.rs
@@ -13,6 +13,19 @@ impl Account {
             balance: 0
         }
     }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+
+        self.balance -= amount;
+        self.balance
+    }
+
+    // fn summary(){}
 }
 
 #[derive(Debug)]
@@ -24,19 +37,44 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![]}
     }
+
+    fn print_accounts(&mut self) {
+        for account in &self.accounts {
+            println!("{:#?}", account);
+        }
+    }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&mut self) -> i32 {
+        let mut bank_balance: i32 = 0;
+        for account in &self.accounts {
+            bank_balance += account.balance;
+        }
+        println!("Bank Total Balance is: {:#?}", bank_balance);
+        bank_balance
+    }
 }
 
 fn print_account(account: &Account) {
     println!("{:#?}", account);
 }
 
-fn make_and_print_account() -> &Account {
-    let account = Account::new(1, String::from("me"));
-    println!("{:#?}", account);
+// fn make_and_print_account() {
+//     let account = Account::new(1, String::from("me"));
+//     println!("{:#?}", account);
 
-    &account
-}
+// }
 fn main() {
-    make_and_print_account();
+    let mut bank = Bank::new();
+
+    let mut account = Account::new(1, String::from("me"));
+
+    account.deposit(500);
+    account.withdraw(250);
+    bank.add_account(account);
+    println!("{:#?}", bank);
 
 }
