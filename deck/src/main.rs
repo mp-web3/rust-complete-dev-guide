77a7afd110fe4bfccdc752f825cdf5532246891a
Mattia <mp.web3@gmail.com>
2025-03-13 19:25:31 +0100

    creating a playful deck

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+use rand::{rng, prelude::SliceRandom};
 #[derive(Debug)]
 
 struct Deck {
@@ -5,28 +7,40 @@ struct Deck {
 }
 
 // Inherent Implementation of Deck
-// Self is a reference to whatever is the parent implementation
 impl Deck {
     fn new() -> Self {
-        let suits = ["Hearts", "Spades", "Diamonds"];
-        let values = ["Ace", "Two", "Three"];
-        let mut cards: Vec<String> = vec![];
-
-        // Double nested for loop to get all possible combinations
-        for suit in suits {
-            for value in values {
-                let card: String = format!("{} of {}", value, suit);
-                cards.push(card);
-            }
+    
+    let suits = ["Hearts", "Spades", "Diamonds"];
+    let values = ["Ace", "Two", "Three"];
+    let mut cards: Vec<String> = vec![];
+    
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
         }
+    }
+    
+    let deck = Deck { cards: cards };
+    return deck;
+    }
 
-        let deck: Deck = Deck { cards };
-        return deck;
+    fn shuffle(&mut self) {
+        let mut rng = rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards:usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
     }
 }
-
 fn main() {
-    let deck = Deck::new();
+    
+    let mut deck = Deck::new();
+    // deck.shuffle();
+    let hand = deck.deal(3);
 
     println!("Here is your deck {:#?}", deck);
+    println!("Here is your hand {:#?}", hand);
+    println!("The deck has {:#?} cards", deck.cards.len());
 }
