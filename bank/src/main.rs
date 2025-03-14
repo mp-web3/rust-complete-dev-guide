8b8c15152034456704329f910ea5b872adecc652
Mattia <mp.web3@gmail.com>
2025-03-14 17:11:57 +0100

    done

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -7,7 +7,25 @@ struct Account {
 
 impl Account {
     fn new(id: u32, holder: String) -> Self {
-        Account { id, balance: 0, holder }
+        Account {
+            id,
+            balance: 0,
+            holder,
+        }
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+
+    fn summary(&self) -> String {
+        format!("Account ID: {}, Holder: {}, Balance: {}", self.id, self.holder, self.balance)
     }
 }
 
@@ -20,8 +38,36 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts.iter().map(|account| account.summary()).collect()
+    }
 }
 
+fn print_account(account: &Account) {
+    println!("{:#?}", account);
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut bank = Bank::new();
+    let account = Account::new(1, String::from("Mattia"));
+    let account2 = Account::new(2, String::from("John"));
+
+    bank.add_account(account);
+    bank.add_account(account2);
+
+    bank.accounts[0].deposit(100);
+    bank.accounts[0].withdraw(30);
+
+    println!("{}", bank.total_balance());
+    println!("{:#?}", bank.summary());
 }
